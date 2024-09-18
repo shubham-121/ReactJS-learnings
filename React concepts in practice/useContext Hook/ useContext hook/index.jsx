d8cfe988da1aba86c31b9a0e56773e15c3ec3027
Shubham Bhatt <81768689+shubham-121@users.noreply.github.com>
2024-09-18 12:39:37 +0530

    Create index.jsx

---
+++ React concepts in practice/useContext Hook/ useContext hook/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+
+import Random from "./testingContext/Random";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+
+
+  <React.StrictMode>
+    <Random></Random>
+  </React.StrictMode>
+);
